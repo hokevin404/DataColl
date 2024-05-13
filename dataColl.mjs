6e6df67c548a57e64fd2f5d99d58c98a067ffc22
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-05-12 22:45:52 -0700

    Completed Part 3

--- dataColl.mjs
+++ dataColl.mjs
@@ -77,18 +77,8 @@ let myArr = [];
 let storedArr = [];
 let counter = 0;
 
-for(let char of stringOfCSV)
-{
-    if(char === '\n')
-    {
-        columns++;
-        break;
-    }
-    else if(char === ',')
-        columns++;
-}
-// console.log(columns);
-
+columns = getColumns(stringOfCSV);
+//console.log(columns);
 
 for(let x of stringOfCSV)
 {
@@ -100,10 +90,10 @@ for(let x of stringOfCSV)
             str='';
             counter++;
             // console.log(myArr);
-            if(counter >= 0)
+            if(columns >= 0)
             {
                 storedArr.push(myArr);
-                counter--;
+                columns--;
             }
             myArr = [];
             break;
@@ -126,7 +116,72 @@ for(let x of stringOfCSV)
     if(counter === stringOfCSV.length)
     {
         myArr.push(str);
-        console.log(myArr);
+        storedArr.push(myArr);
+        //console.log(myArr);
     }     
 }
-console.log(storedArr);
+// console.log(storedArr);
+
+
+// Part 3: Transforming Data
+// While the data is now much more workable than it was in its string format, there is still a large amount of obscurity in the data itself. When we access an arbitrary index of the results array, it is impossible to know what that data is referring to without additional cross-referencing.
+// 
+// In order to make it more obvious what the data is, we will transform our rows into objects.
+// 
+// Implement the following:
+//  ~ For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
+//      ~ Convert these keys to all lowercase letters for consistency.
+//  ~ Store these objects in an array, in the order that they were originally listed.
+//  ~ Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
+// 
+// For instance, the results of the example data above being passed through this step are as follows:
+//      [["ID", "Name", "Occupation", "Age"],
+//       ["42", "Bruce", "Knight", "41"],
+//       ["57", "Bob", "Fry Cook", "19"],
+//       ["63", "Blaine", "Quiz Master", "58"],
+//       ["98", "Bill", "Doctor’s Assistant", "26"]]
+// 
+// becomes
+//
+//      [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+//       { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+//       { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+//       { id: "98", name: "Bill", occupation: "Doctor’s Assistant", age: "26" }]
+//
+// Important: While this functionality can be built into the original CSV parser you built in Part 2, we are intentionally creating two different algorithms to test different skillsets. Please leave these sections separate even if it would be more efficient to combine them.
+
+let finalArr = [];
+
+columns = getColumns(stringOfCSV);
+
+// Change headings to lower case words
+for(let i = 0; i < columns; i++)
+    storedArr[0][i] = storedArr[0][i].toLowerCase();
+
+// Assign headings as keys to the values in table
+for(let j = 1; j < storedArr.length; j++)
+{
+    let finalobj = {};
+    for(let k = 0; k < columns; k++)
+        finalobj[storedArr[0][k]] = storedArr[j][k];
+        
+    finalArr.push(finalobj);
+}
+console.log(finalArr);
+
+// FUNCTIONS---------------------------------------------------------------------------------------
+function getColumns(arr)
+{
+    let col = 0;
+    for(let char of stringOfCSV)
+    {
+        if(char === '\n')
+        {
+            col++;
+            break;
+        }
+        else if(char === ',')
+            col++;
+    }
+    return col;
+}
